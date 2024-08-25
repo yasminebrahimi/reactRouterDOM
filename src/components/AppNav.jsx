a0f1f0b8fc89277d558e3bc93ca67f77ea77733a
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2024-08-25 19:23:05 +0300

    Reviewed and comments added

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,21 +1,35 @@
+// Import the necessary components from the react-router-dom library
 import { Link, NavLink } from "react-router-dom";
 
+// Define the AppNav functional component
 function AppNav() {
   return (
-<nav>
-  <ul>
-    <li>
-      <NavLink to="/home">Home</NavLink>
-    </li>
-    <li>
-      <NavLink to="/posts">Posts</NavLink>
-    </li>
-    <li>
-      <NavLink to="/dashboard">Dashbord</NavLink>
-    </li>
-  </ul>
-</nav>
-  )
+    // Return a navigation bar (nav element)
+    <nav>
+      {/* Unordered list to hold the navigation links */}
+      <ul>
+        {/* List item for the Home link */}
+        <li>
+          {/* NavLink is used for navigation; the 'to' prop specifies the target route */}
+          {/* NavLink adds an active class to the link when it's the current route */}
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        {/* List item for the Posts link */}
+        <li>
+          {/* NavLink for navigating to the Posts route */}
+          <NavLink to="/posts">Posts</NavLink>
+        </li>
+        {/* List item for the Dashboard link */}
+        <li>
+          {/* NavLink for navigating to the Dashboard route */}
+          {/* Note: There's a typo here in "Dashbord", it should be "Dashboard" */}
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default AppNav; 
+// Export the AppNav component as the default export of this module
+export default AppNav;
+
