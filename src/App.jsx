7252b06c09c4b10a5b4a0a0df345e6d27a0037d3
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2024-09-03 10:12:18 +0300

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-
-import Home from "./pages/Home"; 
-import Posts from "./pages/Posts"; 
-import Dashboard from "./pages/Dashboard"; 
-import AppNav from "./components/AppNav"; 
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import Dashboard from "./pages/Dashboard";
 import Layout from "./components/Layout";
 import Profile from "./components/Profile";
 import Payments from "./components/Payments";
-import Post from "./components/post";
+import Post from "./components/Post";
 import Login from "./pages/Login";
 
 function App() {
@@ -17,13 +14,13 @@ function App() {
     <div>
       <Routes>
         <Route path="/app" element={<Layout />}>
-          <Route index element={<Home />} /> {/* This is the default route under /app */}
-          <Route path="login" element={<Login/>} />
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
           <Route path="posts/:id" element={<Post />} />
           <Route path="posts" element={<Posts />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="profile" element={<Profile />}>
-            {/* Nested routes under profile */}
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route index element={<Navigate to="profile" />} />
+            <Route path="profile" element={<Profile />} />
             <Route path="payments" element={<Payments />} />
           </Route>
         </Route>
@@ -33,3 +30,9 @@ function App() {
 }
 
 export default App;
+
+// programmatically naviagation => same as Link !!
+// 1. useNavigage() => imperative
+// 2. Navigate Component => imperative
+
+// Link => declarative
